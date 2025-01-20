a651181accf4421c69ace1f1a56397723efef78f
Maxj <maxence.1686@gmail.com>
2025-01-20 02:28:38 +0100

    fin sae14

--- src/Controller/PortfolioController.php
+++ src/Controller/PortfolioController.php
@@ -40,4 +40,72 @@ class PortfolioController extends AbstractController
             'controller_name' => 'PortfolioController',
         ]);
     }
+
+    #[Route('/administrer/maitriser_loi_fondamentale_electricite', name: 'app_administrer_loi_elect')]
+    public function maitrise_loi_fondamentale_electrcite(): Response
+    {
+        return $this->render('portfolio/maitriser_loi_fondamentales_electricite.html.twig', [
+            'controller_name' => 'PortfolioController',
+        ]);
+    }
+
+    #[Route('/administrer/configurer_fonction_base_reseau_local', name: 'app_administrer_conf')]
+    public function conf_fonction_base(): Response
+    {
+        return $this->render('portfolio/confi_fonction_base_reseau_local.html.twig', [
+            'controller_name' => 'PortfolioController',
+        ]);
+    }
+
+    #[Route('/administrer/maitrise_systeme_exploitation', name: 'app_administrer_systeme_exploitation')]
+    public function systeme_exploitation(): Response
+    {
+        return $this->render('portfolio/systeme_exploitation.html.twig', [
+            'controller_name' => 'PortfolioController',
+        ]);
+    }
+
+    #[Route('/connecter/deployer_support_transmission', name: 'app_connecter_support_transmission')]
+    public function support_transmission(): Response
+    {
+        return $this->render('portfolio/deployer_support_transmission.html.twig', [
+            'controller_name' => 'PortfolioController',
+        ]);
+    }
+
+    #[Route('/connecter/communication', name: 'app_connecter_communication')]
+    public function communication(): Response
+    {
+        return $this->render('portfolio/comunication.html.twig', [
+            'controller_name' => 'PortfolioController',
+        ]);
+    }
+
+    #[Route('/programmer/utilisation_systeme_informatique&outil', name: 'app_programmer_systeme_info_outil')]
+    public function systeme_info_outil(): Response
+    {
+        return $this->render('portfolio/utilisation_systeme_info_outils.html.twig', [
+            'controller_name' => 'PortfolioController',
+        ]);
+    }
+
+    #[Route('/programmer/architecture_web', name: 'app_programmer_archi_web')]
+    public function archi_web(): Response
+    {
+        return $this->render('portfolio/archi_web.html.twig', [
+            'controller_name' => 'PortfolioController',
+        ]);
+    }
+    
+    #[Route('/download-pdf/{filename}', name: 'download_pdf')]
+    public function downloadPdf(string $filename): Response
+    {
+        $pdfPath = $this->getParameter('kernel.project_dir') . '/public/uploads/' . $filename;
+
+        if (!file_exists($pdfPath)) {
+            throw $this->createNotFoundException('Le fichier demandé est introuvable.');
+        }
+
+        return $this->file($pdfPath);
+    }
 }
